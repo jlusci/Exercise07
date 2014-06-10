202968b0aeb5f76963c2620bf45db35741fd32e8
Hackbright Student <info@hackbrightacademy.com>
2014-06-09 17:23:37 -0700

    used translation table to get rid of all special characters

--- wordcount.py
+++ wordcount.py
@@ -1,28 +1,27 @@
 import sys 
-# import string
+from string import maketrans
 
 def main():
     word_counts = {}
     script, filename = sys.argv
 
-    # t = string.maketrans('--', '  ')
+    intab = '.,;!?_:"()-/'
+    outtab = "            "
+    transtable = maketrans(intab, outtab)
   
     f = open(filename)
     for line in f:
         line = line.rstrip()
         line = line.lower()
-        line = line.translate(None, '.,;!?--_:"()')
+        line = line.translate(transtable)
         word_list = line.split(' ')
 
         for w in word_list:
-            # w = w.translate(None, '.,;!?--_:"()')
             if w in word_counts:
                 word_counts[w] += 1
             else:
                 word_counts[w] = 1
 
-    # for key, value in word_counts.iteritems():
-    #     print key, value
     frequency = {}
     for w in word_counts:
         if word_counts[w] in frequency:
@@ -31,7 +30,9 @@ def main():
             frequency[word_counts[w]] = [w]
 
     for key, value in reversed(sorted(frequency.iteritems())):
-        print key, ", ".join(sorted(value))
+        # print key, ", ".join(sorted(value)), "\n"
+        for v in sorted(value):
+            print v, key
      
 
 if __name__ == "__main__":
